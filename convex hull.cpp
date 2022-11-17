20a1746ae41c4df518266bf494432b0eed7f606c
deekshant-g <95540849+deekshant-g@users.noreply.github.com>
2022-11-17 11:27:39 +0530

    Update convex hull.cpp
    old code was getting only 88 score out of 100, 
    this new one is passing all the test cases.

--- convex hull.cpp
+++ convex hull.cpp
@@ -2,97 +2,80 @@
 Given random points in a 2-D plane, construct a convex polygon with minimum area of covering and 
 which encompasses all the given points.
 */
-#include<bits/stdc++.h>
-int cou = 0;
+#define hello <algorithm>
+#define hello2 <vector>
+#define hello3 <cmath>
+#include<iostream>
+#include hello3
+#include hello2
+#include hello 
 
-struct Point{
-    int x, y;
+using namespace std;
+struct Point
+{
+    int x;
+    int y;
 };
- 
-int orientation(Point p, Point q, Point r){
-    int val = (q.y - p.y) * (r.x - q.x) -
-              (q.x - p.x) * (r.y - q.y);
- 
-    if (val == 0) return 0; 
-    return (val > 0)? 1: 2; 
-}
-
-bool cmp(Point &a, Point &b){
-    if(a.x==b.x&&a.y==b.y)
-        cou++;
-    
-    if(a.x == b.x)
-        return a.y < b.y;
-    else
-        return a.x < b.x;
-}
-
-bool myFunc(Point &a, Point &b){
-    return (a.x==b.x && a.y==b.y);
-}    
- 
-void convexHull(Point *points, int n){
-    cou = 0;
-    if (n < 3){
-        cout << "-1";
-        return;    
-    } 
-    
-    vector<Point> hull;
-    
-    int l = 0;
-    for (int i = 1; i < n; i++)
-        if (points[i].x < points[l].x)
-            l = i;
- 
-    int p = l, q;
-    do{
-        hull.push_back(points[p]);
-        
-        q = (p+1)%n;
-        
-        for (int i = 0; i < n; i++)
-        {
-           if (orientation(points[p], points[i], points[q]) == 2)
-               q = i;
+int orientation(int p, int i, int q, Point points[])
+{
+    int val = (points[i].y - points[p].y)*(points[q].x - points[i].x) - (points[i].x - points[p].x)*(points[q].y - points[i].y);
+    if (val < 0){
+        return true;
+    }
+    else if (val == 0){
+        int dist1 = (points[i].x - points[p].x)*(points[i].x - points[p].x) + (points[i].y - points[p].y)*(points[i].y - points[p].y);
+        int dist2 = (points[q].x - points[p].x)*(points[q].x - points[p].x) + (points[q].y - points[p].y)*(points[q].y - points[p].y);
+        if (dist1 <= dist2){
+            return false;
         }
-        p = q;
- 
-    } while (p != l); 
-    
-    sort(hull.begin(), hull.end(), cmp);
-    
-    auto ip = unique(hull.begin(), hull.end(), myFunc); 
-    
-    hull.resize(std::distance(hull.begin(), ip)); 
-    
-    if(n < 4 && cou > 0 || hull.size() < 3){
-        cout << "-1";
-        return;
+        else{
+            return true;
+        }
     }
     else{
-        for (int i = 0; i < hull.size(); i++){
-            if(i != hull.size() - 1)
-                cout << hull[i].x << " " << hull[i].y << ", ";
-            else
-                cout << hull[i].x << " " << hull[i].y; 
-        }        
+        return false;
     }
 }
- 
-int main(){
-    int t, n;
-    cin >> t;
-    while(t--){
-        cin >> n;
-        Point *points = new Point[n];
-
-        for(int i=0; i<n; i++){
-            cin >> points[i].x >> points[i].y;                
+void convexHull(int n, Point points[])
+{
+    int l = 0, count = 0;
+    if (n < 3){
+        cout << 0;
+    }
+    else{
+        for (int i = 0; i < n; i++)    {
+            if (points[i].x < points[l].x){
+                l = i;
+            }
         }
-        
-        convexHull(points, n);
-        cout << "\n";
+        int p = l, q;
+        do{
+            q = (p + 1) % n;
+            for (int i = 0; i < n; i++){
+                if (orientation(p, i, q, points)){
+                    q = i;
+                }
+            }
+            p = q;
+            count++;
+        } while (p != l);
+        cout << count << endl;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
+    cin >> T;
+    for (test_case = 1; test_case <= T; ++test_case){
+        int n;
+        cin >> n;
+        Point points[n];
+        for (int i = 0; i < n; i++){
+            cin >> points[i].x >> points[i].y;
+        }
+        convexHull(n, points);
     }
     return 0;
 }
